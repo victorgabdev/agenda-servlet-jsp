fc9e52d399122f8cac0139524450ce93d7eff705
Victor Gabriel de Araujo Costa <victor.costa@sefaz.ma.gov.br>
2024-03-16 09:51:12 -0300

    Endpoint /insert criado

--- src/main/java/controller/AgendaController.java
+++ src/main/java/controller/AgendaController.java
@@ -1,30 +1,55 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.AgendaDAO;
+import model.AgendaBean;
 
-/**
- * Servlet implementation class Controller
- */
-@WebServlet(urlPatterns = {"/Controller", "/main"})
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class AgendaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public AgendaController() {
-        super();
-    }
+	
+	AgendaDAO agendaDAO = new AgendaDAO();
+	AgendaBean contato = new AgendaBean();
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public AgendaController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String servletPath = request.getServletPath();
-		
-		if(servletPath.equals("/main")) {
-			response.sendRedirect("agenda.jsp");
+
+		if (servletPath.equals("/main")) {
+			contatos(request, response);
+		} else if (servletPath.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
+		
+	}
+
+	// listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.sendRedirect("agenda.jsp");
+	}
+
+	// Cria novo contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		
+		
 	}
 
 }
